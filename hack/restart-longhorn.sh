86ceb41e42ffb0320be13b15ebc69079f150ac9f
Jory Irving <vetrius@gmail.com>
2023-03-16 12:51:46 -0600

    add restart script;

---
+++ hack/restart-longhorn.sh
@@ -0,0 +1,10 @@
+kubectl rollout restart daemonset engine-image-ei-d4c780c6 -n storage
+kubectl rollout restart daemonset longhorn-csi-plugin -n storage
+kubectl rollout restart daemonset longhorn-manager -n storage
+
+kubectl rollout restart deploy csi-attacher -n storage
+kubectl rollout restart deploy csi-provisioner -n storage
+kubectl rollout restart deploy csi-resizer -n storage
+kubectl rollout restart deploy csi-snapshotter -n storage
+kubectl rollout restart deploy longhorn-driver-deployer -n storage
+kubectl rollout restart deploy longhorn-ui -n storage
