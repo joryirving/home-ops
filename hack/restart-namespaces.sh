cec275e7df0eac950bb7fd7ede01d659295a6222
Jory Irving <jory@jory.dev>
2023-03-18 20:10:29 -0600

    add new hack script

---
+++ hack/restart-namespaces.sh
@@ -0,0 +1,8 @@
+kubectl get deployments -n vpn -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart deployment $LINE -n vpn; done;
+kubectl get statefulset -n vpn -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart statefulset $LINE -n vpn; done;
+
+kubectl get deployments -n default -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart deployment $LINE -n default; done;
+kubectl get statefulset -n default -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart statefulset $LINE -n default; done;
+
+kubectl get deployments -n media -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart deployment $LINE -n media; done;
+kubectl get statefulset -n media -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart statefulset $LINE -n media; done;
