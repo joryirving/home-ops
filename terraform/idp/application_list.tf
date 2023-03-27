b0877d6a28b325dfb704619343b0db4118d883df
Jory Irving <vetrius@gmail.com>
2023-03-27 09:40:36 -0600

    update tf

--- terraform/idp/application_list.tf
+++ terraform/idp/application_list.tf
@@ -4,3 +4,23 @@ locals {
     "radarr"
   ])
 }
+
+resource "authentik_provider_proxy" "proxy" {
+  for_each           = local.applications
+  name               = each.value
+  internal_host      = "http://${each.value}.media"
+  external_host      = "http://${each.value}.lildrunkensmurf.com"
+  authorization_flow = data.authentik_flow.default-authorization-flow.id
+}
+
+resource "authentik_application" "application" {
+  for_each           = local.applications
+  name               = authentik_provider_proxy.proxy[each.value].name
+  slug               = authentik_provider_proxy.proxy[each.value].name
+  protocol_provider  = authentik_provider_proxy.proxy[each.value].id
+  group              = authentik_group.media.name
+  open_in_new_tab    = true
+  meta_icon          = "https://${each.value}.lildrunkensmurf.com/Content/Images/logo.svg"
+  meta_launch_url    = "https://${each.value}.lildrunkensmurf.com"
+  policy_engine_mode = "all"
+}
--- terraform/idp/auth_media.tf
+++ terraform/idp/auth_media.tf
@@ -19,49 +19,16 @@ resource "authentik_source_oauth" "discord" {
   consumer_secret = var.client_secret
 }
 
-resource "authentik_provider_proxy" "proxy" {
-  for_each = local.applications
-  name     = each.value
-  #internal_host      = "http://sonarr.media"
-  external_host      = "http://${each.value}.lildrunkensmurf.com"
-  authorization_flow = data.authentik_flow.default-authorization-flow.id
+resource "authentik_service_connection_kubernetes" "local" {
+  name  = "local"
+  local = true
 }
 
-resource "authentik_application" "application" {
-  for_each           = local.applications
-  name               = authentik_provider_proxy.proxy[each.value].name
-  slug               = authentik_provider_proxy.proxy[each.value].name
-  protocol_provider  = authentik_provider_proxy.proxy[each.value].id
-  group              = authentik_group.media.name
-  open_in_new_tab    = true
-  meta_icon          = "https://${each.value}.lildrunkensmurf.com/Content/Images/logo.svg"
-  meta_launch_url    = "https://${each.value}.lildrunkensmurf.com"
-  policy_engine_mode = "all"
+resource "authentik_outpost" "media_outpost" {
+  name               = "media-outpost"
+  type               = "proxy"
+  service_connection = authentik_service_connection_kubernetes.local.id
+  protocol_providers = [
+    for s in local.applications : authentik_provider_proxy.proxy[s].id
+  ]
 }
-
-# resource "authentik_provider_proxy" "radarr" {
-#   name = "radarr"
-#   #internal_host      = "http://radarr.media"
-#   external_host      = "http://radarr.lildrunkensmurf.com"
-#   authorization_flow = data.authentik_flow.default-authorization-flow.id
-# }
-
-# resource "authentik_application" "radarr" {
-#   name               = authentik_provider_proxy.radarr.name
-#   slug               = authentik_provider_proxy.radarr.name
-#   protocol_provider  = authentik_provider_proxy.radarr.id
-#   group              = authentik_group.media.name
-#   open_in_new_tab    = true
-#   meta_icon          = "https://radarr.lildrunkensmurf.com/Content/Images/logo.svg"
-#   meta_launch_url    = "https://radarr.lildrunkensmurf.com"
-#   policy_engine_mode = "all"
-# }
-
-# resource "authentik_outpost" "media_outpost" {
-#   name = "media-outpost"
-#   type = "proxy"
-#   protocol_providers = [
-#     authentik_provider_oauth2.sonarr.id,
-#     authentik_provider_oauth2.radarr.id
-#   ]
-# }
