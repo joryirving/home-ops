ecb66c7fc25a54dc4acde84df329c34cf89cb2f5
Jory Irving <jory@jory.dev>
2023-03-27 19:14:55 -0600

    add branding

--- terraform/idp/system.tf
+++ terraform/idp/system.tf
@@ -3,11 +3,11 @@ data "authentik_certificate_key_pair" "generated" {
 }
 
 resource "authentik_tenant" "home" {
-  domain         = data.sops_file.authentik_secrets.data["cluster_domain"]
-  default        = false
-  branding_title = "Home"
-  #branding_logo    = "placeholder.svg"
-  #branding_favicon = "placeholder.png"
+  domain           = data.sops_file.authentik_secrets.data["cluster_domain"]
+  default          = false
+  branding_title   = "Home"
+  branding_logo    = "/static/dist/assets/icons/icon_left_brand.svg"
+  branding_favicon = "/static/dist/assets/icons/icon.png"
 
   flow_authentication = authentik_flow.authentication.uuid
   flow_invalidation   = authentik_flow.invalidation.uuid
