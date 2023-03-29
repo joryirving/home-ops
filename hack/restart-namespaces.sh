51842fe4c99ec308a530175c09e30087d2646219
Jory Irving <vetrius@gmail.com>
2023-03-29 10:07:56 -0600

    update scripts

--- hack/restart-namespaces.sh
+++ hack/restart-namespaces.sh
@@ -6,3 +6,6 @@ kubectl get statefulset -n default -o custom-columns=NAME:.metadata.name|grep -i
 
 kubectl get deployments -n media -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart deployment $LINE -n media; done;
 kubectl get statefulset -n media -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart statefulset $LINE -n media; done;
+
+kubectl get deployments -n downloads -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart deployment $LINE -n downloads; done;
+kubectl get statefulset -n downloads -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart statefulset $LINE -n downloads; done;
