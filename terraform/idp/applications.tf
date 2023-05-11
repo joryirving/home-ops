203763e638c656753d6818efea557bb1f9fd612c
Jory Irving <vetrius@gmail.com>
2023-05-11 08:40:02 -0600

    /Another

--- terraform/idp/applications.tf
+++ terraform/idp/applications.tf
@@ -1,118 +1,118 @@
-locals {
-  media_applications = toset([
-    "overseerr",
-    "tautulli",
-  ])
+# locals {
+#   media_applications = toset([
+#     "overseerr",
+#     "tautulli",
+#   ])
 
-  download_applications = toset([
-    "bazarr",
-    "prowlarr",
-    "qbittorrent",
-    "radarr",
-    "readarr",
-    "sabnzbd",
-    "sonarr",
-  ])
+#   download_applications = toset([
+#     "bazarr",
+#     "prowlarr",
+#     "qbittorrent",
+#     "radarr",
+#     "readarr",
+#     "sabnzbd",
+#     "sonarr",
+#   ])
 
-  infra_applications = toset([
-    "longhorn",
-    "nas",
-    "tdarr"
-  ])
+#   infra_applications = toset([
+#     "longhorn",
+#     "nas",
+#     "tdarr"
+#   ])
 
-  proxy_list = concat(
-    values(authentik_provider_proxy.download_proxy)[*].id,
-    values(authentik_provider_proxy.media_proxy)[*].id,
-    values(authentik_provider_proxy.infra_proxy)[*].id,
-    [authentik_provider_proxy.hass_proxy.id]
-  )
-}
+#   proxy_list = concat(
+#     values(authentik_provider_proxy.download_proxy)[*].id,
+#     values(authentik_provider_proxy.media_proxy)[*].id,
+#     values(authentik_provider_proxy.infra_proxy)[*].id,
+#     [authentik_provider_proxy.hass_proxy.id]
+#   )
+# }
 
 ### Proxy Providers ###
 ## Downloads ##
-resource "authentik_provider_proxy" "download_proxy" {
-  for_each              = local.download_applications
-  name                  = "${each.value}-provider"
-  external_host         = "http://${each.value}.${data.sops_file.authentik_secrets.data["cluster_domain"]}"
-  mode                  = "forward_single"
-  authorization_flow    = resource.authentik_flow.provider-authorization-implicit-consent.uuid
-  access_token_validity = "hours=4"
-}
+# resource "authentik_provider_proxy" "download_proxy" {
+#   for_each              = local.download_applications
+#   name                  = "${each.value}-provider"
+#   external_host         = "http://${each.value}.${data.sops_file.authentik_secrets.data["cluster_domain"]}"
+#   mode                  = "forward_single"
+#   authorization_flow    = resource.authentik_flow.provider-authorization-implicit-consent.uuid
+#   access_token_validity = "hours=4"
+# }
 
-resource "authentik_application" "download_application" {
-  for_each           = local.download_applications
-  name               = title(each.value)
-  slug               = authentik_provider_proxy.download_proxy[each.value].name
-  protocol_provider  = authentik_provider_proxy.download_proxy[each.value].id
-  group              = authentik_group.downloads.name
-  open_in_new_tab    = true
-  meta_icon          = "https://raw.githubusercontent.com/LilDrunkenSmurf/k3s-home-cluster/main/icons/${each.value}.png"
-  policy_engine_mode = "all"
-}
+# resource "authentik_application" "download_application" {
+#   for_each           = local.download_applications
+#   name               = title(each.value)
+#   slug               = authentik_provider_proxy.download_proxy[each.value].name
+#   protocol_provider  = authentik_provider_proxy.download_proxy[each.value].id
+#   group              = authentik_group.downloads.name
+#   open_in_new_tab    = true
+#   meta_icon          = "https://raw.githubusercontent.com/LilDrunkenSmurf/k3s-home-cluster/main/icons/${each.value}.png"
+#   policy_engine_mode = "all"
+# }
 
 ## Infra ##
-resource "authentik_provider_proxy" "infra_proxy" {
-  for_each              = local.infra_applications
-  name                  = "${each.value}-provider"
-  external_host         = "http://${each.value}.${data.sops_file.authentik_secrets.data["cluster_domain"]}"
-  mode                  = "forward_single"
-  authorization_flow    = resource.authentik_flow.provider-authorization-implicit-consent.uuid
-  access_token_validity = "hours=4"
-}
+# resource "authentik_provider_proxy" "infra_proxy" {
+#   for_each              = local.infra_applications
+#   name                  = "${each.value}-provider"
+#   external_host         = "http://${each.value}.${data.sops_file.authentik_secrets.data["cluster_domain"]}"
+#   mode                  = "forward_single"
+#   authorization_flow    = resource.authentik_flow.provider-authorization-implicit-consent.uuid
+#   access_token_validity = "hours=4"
+# }
 
-resource "authentik_application" "infra_application" {
-  for_each           = local.infra_applications
-  name               = title(each.value)
-  slug               = authentik_provider_proxy.infra_proxy[each.value].name
-  protocol_provider  = authentik_provider_proxy.infra_proxy[each.value].id
-  group              = authentik_group.infrastructure.name
-  open_in_new_tab    = true
-  meta_icon          = "https://raw.githubusercontent.com/LilDrunkenSmurf/k3s-home-cluster/main/icons/${each.value}.png"
-  policy_engine_mode = "all"
-}
+# resource "authentik_application" "infra_application" {
+#   for_each           = local.infra_applications
+#   name               = title(each.value)
+#   slug               = authentik_provider_proxy.infra_proxy[each.value].name
+#   protocol_provider  = authentik_provider_proxy.infra_proxy[each.value].id
+#   group              = authentik_group.infrastructure.name
+#   open_in_new_tab    = true
+#   meta_icon          = "https://raw.githubusercontent.com/LilDrunkenSmurf/k3s-home-cluster/main/icons/${each.value}.png"
+#   policy_engine_mode = "all"
+# }
 
 ## Media ##
-resource "authentik_provider_proxy" "media_proxy" {
-  for_each                      = local.media_applications
-  name                          = "${each.value}-provider"
-  basic_auth_enabled            = true
-  basic_auth_username_attribute = data.sops_file.authentik_secrets.data["${each.value}_username"]
-  basic_auth_password_attribute = data.sops_file.authentik_secrets.data["${each.value}_password"]
-  external_host                 = "http://${each.value}.${data.sops_file.authentik_secrets.data["cluster_domain"]}"
-  mode                          = "forward_single"
-  authorization_flow            = resource.authentik_flow.provider-authorization-implicit-consent.uuid
-  access_token_validity         = "hours=4"
-}
+# resource "authentik_provider_proxy" "media_proxy" {
+#   for_each                      = local.media_applications
+#   name                          = "${each.value}-provider"
+#   basic_auth_enabled            = true
+#   basic_auth_username_attribute = data.sops_file.authentik_secrets.data["${each.value}_username"]
+#   basic_auth_password_attribute = data.sops_file.authentik_secrets.data["${each.value}_password"]
+#   external_host                 = "http://${each.value}.${data.sops_file.authentik_secrets.data["cluster_domain"]}"
+#   mode                          = "forward_single"
+#   authorization_flow            = resource.authentik_flow.provider-authorization-implicit-consent.uuid
+#   access_token_validity         = "hours=4"
+# }
 
-resource "authentik_application" "media_application" {
-  for_each           = local.media_applications
-  name               = title(each.value)
-  slug               = authentik_provider_proxy.media_proxy[each.value].name
-  protocol_provider  = authentik_provider_proxy.media_proxy[each.value].id
-  group              = authentik_group.media.name
-  open_in_new_tab    = true
-  meta_icon          = "https://raw.githubusercontent.com/LilDrunkenSmurf/k3s-home-cluster/main/icons/${each.value}.png"
-  policy_engine_mode = "all"
-}
+# resource "authentik_application" "media_application" {
+#   for_each           = local.media_applications
+#   name               = title(each.value)
+#   slug               = authentik_provider_proxy.media_proxy[each.value].name
+#   protocol_provider  = authentik_provider_proxy.media_proxy[each.value].id
+#   group              = authentik_group.media.name
+#   open_in_new_tab    = true
+#   meta_icon          = "https://raw.githubusercontent.com/LilDrunkenSmurf/k3s-home-cluster/main/icons/${each.value}.png"
+#   policy_engine_mode = "all"
+# }
 
 ## HASS ##
-resource "authentik_provider_proxy" "hass_proxy" {
-  name                  = "home-assistant-provider"
-  external_host         = "http://hass.${data.sops_file.authentik_secrets.data["cluster_domain"]}"
-  mode                  = "forward_single"
-  authorization_flow    = resource.authentik_flow.provider-authorization-implicit-consent.uuid
-  access_token_validity = "hours=4"
-}
+# resource "authentik_provider_proxy" "hass_proxy" {
+#   name                  = "home-assistant-provider"
+#   external_host         = "http://hass.${data.sops_file.authentik_secrets.data["cluster_domain"]}"
+#   mode                  = "forward_single"
+#   authorization_flow    = resource.authentik_flow.provider-authorization-implicit-consent.uuid
+#   access_token_validity = "hours=4"
+# }
 
-resource "authentik_application" "hass_application" {
-  name               = "Home-Assistant"
-  slug               = authentik_provider_proxy.hass_proxy.name
-  protocol_provider  = authentik_provider_proxy.hass_proxy.id
-  group              = authentik_group.home.name
-  open_in_new_tab    = true
-  meta_icon          = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/png/home-assistant.png"
-  policy_engine_mode = "all"
-}
+# resource "authentik_application" "hass_application" {
+#   name               = "Home-Assistant"
+#   slug               = authentik_provider_proxy.hass_proxy.name
+#   protocol_provider  = authentik_provider_proxy.hass_proxy.id
+#   group              = authentik_group.home.name
+#   open_in_new_tab    = true
+#   meta_icon          = "https://raw.githubusercontent.com/walkxcode/dashboard-icons/main/png/home-assistant.png"
+#   policy_engine_mode = "all"
+# }
 
 ### Oauth2 Providers ###
 ## Grafana ##
@@ -160,29 +160,29 @@ resource "authentik_application" "portainer_application" {
 }
 
 ### Outpost ###
-resource "authentik_outpost" "proxyoutpost" {
-  name               = "proxy-outpost"
-  type               = "proxy"
-  service_connection = authentik_service_connection_kubernetes.local.id
-  protocol_providers = local.proxy_list
-  config = jsonencode({
-    authentik_host          = "https://authentik.${data.sops_file.authentik_secrets.data["cluster_domain"]}",
-    authentik_host_insecure = false,
-    authentik_host_browser  = "",
-    log_level               = "debug",
-    object_naming_template  = "ak-outpost-%(name)s",
-    docker_network          = null,
-    docker_map_ports        = true,
-    docker_labels           = null,
-    container_image         = null,
-    kubernetes_replicas     = 1,
-    kubernetes_namespace    = "security",
-    kubernetes_ingress_annotations = {
-      "cert-manager.io/cluster-issuer" = "letsencrypt-production"
-    },
-    kubernetes_ingress_secret_name = "proxy-outpost-tls",
-    kubernetes_service_type        = "ClusterIP",
-    kubernetes_disabled_components = [],
-    kubernetes_image_pull_secrets  = []
-  })
-}
+# resource "authentik_outpost" "proxyoutpost" {
+#   name               = "proxy-outpost"
+#   type               = "proxy"
+#   service_connection = authentik_service_connection_kubernetes.local.id
+#   protocol_providers = local.proxy_list
+#   config = jsonencode({
+#     authentik_host          = "https://authentik.${data.sops_file.authentik_secrets.data["cluster_domain"]}",
+#     authentik_host_insecure = false,
+#     authentik_host_browser  = "",
+#     log_level               = "debug",
+#     object_naming_template  = "ak-outpost-%(name)s",
+#     docker_network          = null,
+#     docker_map_ports        = true,
+#     docker_labels           = null,
+#     container_image         = null,
+#     kubernetes_replicas     = 1,
+#     kubernetes_namespace    = "security",
+#     kubernetes_ingress_annotations = {
+#       "cert-manager.io/cluster-issuer" = "letsencrypt-production"
+#     },
+#     kubernetes_ingress_secret_name = "proxy-outpost-tls",
+#     kubernetes_service_type        = "ClusterIP",
+#     kubernetes_disabled_components = [],
+#     kubernetes_image_pull_secrets  = []
+#   })
+# }
