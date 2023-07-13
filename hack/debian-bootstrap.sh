27555ce7f46999ee7e280987b03f5212fbc568d7
Jory <jory@jory.dev>
2023-07-13 05:54:23 -0600

    update script

--- hack/debian-bootstrap.sh
+++ hack/debian-bootstrap.sh
@@ -1,4 +1,4 @@
 ## List of packages required on a fresh debian install
 apt update
 apt upgrade -y
-apt install python3 python3-kubernetes open-iscsi nfs-common util-linux iptables -y
+apt install python3 python3-kubernetes open-iscsi nfs-common util-linux iptables intel-media-va-driver-non-free -y
