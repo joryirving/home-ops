6bb79e8465c26fef2ae3da713716efe322eefd72
Jory Irving <vetrius@gmail.com>
2023-07-18 21:02:47 -0600

    add email

--- terraform/minio/providers.tf
+++ terraform/minio/providers.tf
@@ -2,6 +2,7 @@ provider "bitwarden" {
   master_password = data.sops_file.bw_secrets.data["bw_password"]
   client_id       = data.sops_file.bw_secrets.data["bw_client_id"]
   client_secret   = data.sops_file.bw_secrets.data["bw_client_secret"]
+  email           = data.sops_file.bw_secrets.data["bw_email"]
 }
 
 provider "minio" {
