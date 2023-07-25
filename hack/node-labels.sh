f966b7f2e14839f6a2165ffe07e98adbb72f9c61
Jory <jory@jory.dev>
2023-07-25 06:07:32 -0600

    update labels

--- hack/node-labels.sh
+++ hack/node-labels.sh
@@ -1,13 +1,10 @@
 # Label control planes
-## PreferNoSchedule
-kubectl taint nodes raiden node-role.kubernetes.io/master=true:PreferNoSchedule
 
 ## NoSchedule
-kubectl taint nodes zhongli nahida node-role.kubernetes.io/control-plane=true:NoSchedule
+kubectl taint nodes raiden nahida node-role.kubernetes.io/control-plane=true:NoSchedule
 
 # Label workers
 kubectl label nodes eula ayaka node-role.kubernetes.io/worker=true
-kubectl label nodes yelan node-role.kubernetes.io/light-worker=true
 
 # Label Longhorn nodes
 kubectl label nodes eula ayaka yelan node.longhorn.io/create-default-disk=true
