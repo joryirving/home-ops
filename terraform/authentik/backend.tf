a48e58075eed0ebd5d1cd4f222afe38d7659edfd
Jory Irving <vetrius@gmail.com>
2023-08-05 20:00:10 -0600

    remove

--- terraform/authentik/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "kubernetes" {
-    secret_suffix     = "state"
-    in_cluster_config = true
-    namespace         = "flux-system"
-    #config_path   = "~/.kube/config"
-  }
-}
