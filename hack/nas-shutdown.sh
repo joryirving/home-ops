105d66be9dbebac7c0d519cc8df72ab6cf3eee28
Jory <jory@jory.dev>
2023-08-18 10:50:50 -0600

    fix: remove duplicate line

--- hack/nas-shutdown.sh
+++ hack/nas-shutdown.sh
@@ -1,4 +1,3 @@
 #!/usr/bin/env bash
 NAMESPACE=$1
 kubectl get statefulset -n $NAMESPACE -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl scale --replicas=0 sts/$LINE -n $NAMESPACE; done;
-kubectl get statefulset -n $NAMESPACE -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl scale --replicas=0 sts/$LINE -n $NAMESPACE; done;
--- hack/nas-startup.sh
+++ hack/nas-startup.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 NAMESPACE=$1
 kubectl get statefulset -n $NAMESPACE -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl scale --replicas=1 sts/$LINE -n $NAMESPACE; done;
-kubectl get statefulset -n $NAMESPACE -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl scale --replicas=1 sts/$LINE -n $NAMESPACE; done;
 
 #Restart Daemonset
 kubectl get daemonset -n $NAMESPACE -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl rollout restart daemonset $LINE -n $NAMESPACE; done;
