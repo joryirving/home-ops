bf83146f3dd11b45caae12bf7dd44a61f22398d6
Jory Irving <jory@jory.dev>
2023-11-23 14:35:10 -0700

    feat(update): hack scripts
    Signed-off-by: Jory Irving <jory@jory.dev>

--- hack/nas-shutdown.sh
+++ hack/nas-shutdown.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-NAMESPACE=$1
+NAMESPACE=${1:-default}
 kubectl get statefulset -n $NAMESPACE -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl scale --replicas=0 sts/$LINE -n $NAMESPACE; done;
+kubectl get deployment -n $NAMESPACE -o custom-columns=NAME:.metadata.name|grep -iv NAME|while read LINE; do kubectl scale --replicas=0 deployment/$LINE -n $NAMESPACE; done;
