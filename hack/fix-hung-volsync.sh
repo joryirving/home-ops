a95c7531dfb24292bbd085b8cd6c50bac675dcf6
Jory Irving <jory@jory.dev>
2023-12-04 07:40:56 -0700

    chore: update script

--- hack/fix-hung-volsync.sh
+++ hack/fix-hung-volsync.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
-kubectl get job -A -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace | awk '/src/ {print $1, $2}' | while read -r job namespace; do kubectl delete job "$job" -n "$namespace"; done
+kubectl get pvc -A -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace | awk '/src/ {print $1, $2}' | while read -r pvc namespace; do kubectl patch pvc "$pvc" -n "$namespace" -p '{"metadata":{"finalizers":null}}' --type=merge; done
 kubectl get pvc -A -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace | awk '/src/ {print $1, $2}' | while read -r pvc namespace; do kubectl delete pvc "$pvc" -n "$namespace"; done
+kubectl get job -A -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace | awk '/src/ {print $1, $2}' | while read -r job namespace; do kubectl delete job "$job" -n "$namespace"; done
