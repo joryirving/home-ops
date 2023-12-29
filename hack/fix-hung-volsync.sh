4ee2e164ee484699aaeb656f3279552cfe227b40
Jory Irving <46251616+LilDrunkenSmurf@users.noreply.github.com>
2023-12-29 15:38:43 -0700

    Migrate storage (#1336)
    * migrate from local-path to d-csi
    * remove longhorn
    * move snapshot controller
    * migrate to rook
    * Update references to longhorn
    * update ansible
    * add ceph dashboard
    * remove longhorn scripts

--- hack/fix-hung-volsync.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cluster=${1:-default}
-kubectl get job -A -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace --context $cluster | awk '/src/ {print $1, $2}' | while read -r job namespace; do kubectl delete job "$job" -n "$namespace" --context $cluster; done
-kubectl get pvc -A -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace --context $cluster | awk '/src/ {print $1, $2}' | while read -r pvc namespace; do kubectl patch pvc "$pvc" -n "$namespace" -p '{"metadata":{"finalizers":null}}' --type=merge --context $cluster; done
-kubectl get pvc -A -o custom-columns=NAME:.metadata.name,NAMESPACE:.metadata.namespace --context $cluster | awk '/src/ {print $1, $2}' | while read -r pvc namespace; do kubectl delete pvc "$pvc" -n "$namespace" --context $cluster; done
