aa8d34b9d41ec7336db2af12b7d5834bd44cbf64
Jory Irving <jory@jory.dev>
2024-01-19 22:39:07 -0700

    fix: script

--- hack/restart-all-pods.sh
+++ hack/restart-all-pods.sh
@@ -4,6 +4,6 @@
 cluster=${1:-default}
 for ns in $(kubectl get ns -o jsonpath='{.items[*].metadata.name}' --context $cluster); do
   for kind in deploy daemonset statefulset; do
-    kubectl get "${kind}" -n "${ns}" -o name | xargs -I {} kubectl rollout restart {} -n "${ns}" --context $cluster
+    kubectl get "${kind}" -n "${ns}" -o name  --context $cluster | xargs -I {} kubectl rollout restart {} -n "${ns}" --context $cluster
   done
 done
