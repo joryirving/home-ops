f5a8dc284a2f5b781a4d332b8ce3b198d9b187cd
Jory Irving <jory@jory.dev>
2024-03-01 08:42:58 -0700

    feat: update talconfig

---
+++ hack/debian-bootstrap.sh
@@ -0,0 +1,4 @@
+## List of packages required on a fresh debian install
+apt update
+apt upgrade -y
+apt install python3 python3-kubernetes open-iscsi nfs-common util-linux iptables intel-media-va-driver-non-free -y
