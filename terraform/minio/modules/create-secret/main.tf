7c98ad922b1cceba8feef34f6dd7999ec793bed8
Jory Irving <jory@jory.dev>
2024-10-10 09:41:43 -0600

    feat: fix creds

--- terraform/minio/modules/create-secret/main.tf
+++ terraform/minio/modules/create-secret/main.tf
@@ -2,5 +2,5 @@ resource "bitwarden_secret" "item" {
   key        = var.name
   note       = "Token for ${var.name}"
   project_id = var.bw_proj_id
-  value      = "AWS_ACCESS_KEY_ID=${var.username}\nAWS_SECRET_ACCESS_KEY=${var.password}"
+  value      = "AWS_ACCESS_KEY_ID: ${var.username}\nAWS_SECRET_ACCESS_KEY: ${var.password}"
 }
