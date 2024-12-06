1b8cd08900364acd4f593b09604f94a659a8e318
Jory Irving <jory@jory.dev>
2024-12-06 14:22:55 -0700

    feat: update script

--- hack/cert-extract.sh
+++ hack/cert-extract.sh
@@ -1,10 +1,23 @@
 #!/bin/zsh
 export TLS_CERT="jory.dev-tls"
-export TLS_NAMESPACE="cert-manager"
+export TLS_NAMESPACE="network"
 CERT_DIR=/tmp/cert
 CERT_TMP=$CERT_DIR/tmp
 CERT_JSON=$CERT_DIR/certificate/certificate-tls.json
-CLUSTER=${1:-main}
+CLUSTER=${1:-utility}
+DESTINATION=${2:-caddy}
+
+# Set default values based on DESTINATION
+if [[ "$DESTINATION" == "caddy" ]]; then
+  SERVER="root@voyager"
+  PATH="/mnt/cache/docker/CaddyV2/data"
+elif [[ "$DESTINATION" == "unifi" ]]; then
+  SERVER="root@192.168.1.1"
+  PATH="/data/unifi-core/config"
+else
+  echo "Unknown DESTINATION: $DESTINATION"
+  exit 1
+fi
 
 mkdir -p $CERT_DIR/certificate/
 
@@ -24,9 +37,11 @@ if [[ "$DIFF" != "" ]]; then
   kubectl --context $CLUSTER get secret $TLS_CERT -n $TLS_NAMESPACE -ojsonpath="{.data}" | jq '.["tls.key"]' | sed -e s/\"//g | base64 -d > $CERT_DIR/certificate.key
   cp $CERT_TMP $CERT_JSON
   cat $CERT_DIR/certificate.crt $CERT_DIR/certificate.key > $CERT_DIR/certificate.pem
-  scp $CERT_DIR/certificate.crt root@192.168.1.1:/data/unifi-core/config/unifi-core.crt
-  scp $CERT_DIR/certificate.key root@192.168.1.1:/data/unifi-core/config/unifi-core.key
+  scp $CERT_DIR/certificate.crt $SERVER:$PATH
+  scp $CERT_DIR/certificate.key $SERVER:$PATH
+  echo "Certificate copied to $DESTINATION"
 else
   echo "No change in certificates"
 fi
+
 rm $CERT_TMP
