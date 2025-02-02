63532df4d8242bc76bf404481ee2b9d4d0df0db7
Jory Irving <jory@jory.dev>
2025-02-02 12:44:42 -0700

    feat(helmfile): bashing a cilium postsync script

---
+++ kubernetes/shared/bootstrap/scripts/cilium-postsync.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+wait_for_crds() {
+    local crds=(
+        "ciliuml2announcementpolicies.cilium.io"
+        "ciliumbgppeeringpolicies.cilium.io"
+        "ciliumloadbalancerippools.cilium.io"
+    )
+    for crd in "${crds[@]}"; do
+        until kubectl get crd "$crd" &>/dev/null; do
+            sleep 5
+        done
+    done
+}
+apply_kustomize_config() {
+    kubectl apply \
+        --context ${CLUSTER} \
+        --namespace=kube-system \
+        --server-side \
+        --field-manager=kustomize-controller \
+        --kustomize \
+        "${CLUSTER_DIR}/apps/kube-system/cilium/config"
+}
+main() {
+    wait_for_crds
+    apply_kustomize_config
+}
+main
