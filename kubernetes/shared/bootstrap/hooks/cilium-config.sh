2c1cffbb26b17b80e677b72831cddf76f61b3bce
Jory Irving <jory@jory.dev>
2025-02-11 04:08:15 -0700

    feat(helmfile): too soon jr. consolidate bootstrap

--- kubernetes/shared/bootstrap/hooks/cilium-config.sh
+++ kubernetes/shared/bootstrap/hooks/cilium-config.sh
@@ -1,38 +1,42 @@
 #!/usr/bin/env bash
+
 set -euo pipefail
+
+function log() {
+    echo -e "\033[0;32m[$(date --iso-8601=seconds)] (${FUNCNAME[1]}) $*\033[0m"
+}
+
 function wait_for_crds() {
     local -r crds=(
         "ciliuml2announcementpolicies.cilium.io"
         "ciliumbgppeeringpolicies.cilium.io"
         "ciliumloadbalancerippools.cilium.io"
     )
+
     for crd in "${crds[@]}"; do
         until kubectl --context ${CLUSTER} get crd "$crd" &>/dev/null; do
-            echo "Waiting for CRD '${crd}'..."
+            log "Waiting for Cilium CRD '${crd}'..."
             sleep 5
         done
     done
 }
+
 function apply_config() {
-    echo "Checking if Cilium config needs to be applied..."
-    if kubectl --context ${CLUSTER} diff \
-        --namespace=kube-system \
-        --kustomize \
+    if kubectl --context ${CLUSTER} --namespace kube-system diff --kustomize \
         "${CLUSTER_DIR}/apps/kube-system/cilium/config" &>/dev/null;
     then
-        echo "Cilium config is up to date. Skipping..."
+        log "Cilium config is up-to-date. Skipping..."
     else
-        echo "Applying Cilium config..."
-        kubectl --context ${CLUSTER} apply \
-            --namespace=kube-system \
-            --server-side \
-            --field-manager=kustomize-controller \
-            --kustomize \
-            "${CLUSTER_DIR}/apps/kube-system/cilium/config"
+        log "Applying Cilium config..."
+        kubectl --context ${CLUSTER} apply --namespace kube-system --server-side \
+            --field-manager kustomize-controller \
+            --kustomize "${CLUSTER_DIR}/apps/kube-system/cilium/config"
     fi
 }
+
 function main() {
     wait_for_crds
     apply_config
 }
+
 main "$@"
