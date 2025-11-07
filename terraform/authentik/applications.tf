141da7267b63bb287a0dff6b5da6b306f26ac13a
Jory Irving <jory@jory.dev>
2025-11-07 11:51:13 -0700

    fix: add email_verified for romm

--- terraform/authentik/applications.tf
+++ terraform/authentik/applications.tf
@@ -88,14 +88,17 @@ locals {
 }
 
 resource "authentik_provider_oauth2" "oauth2" {
-  for_each              = local.applications
-  name                  = each.key
-  client_id             = each.value.client_id
-  client_secret         = each.value.client_secret
-  authorization_flow    = authentik_flow.provider-authorization-implicit-consent.uuid
-  authentication_flow   = authentik_flow.authentication.uuid
-  invalidation_flow     = data.authentik_flow.default-provider-invalidation-flow.id
-  property_mappings     = data.authentik_property_mapping_provider_scope.oauth2.ids
+  for_each            = local.applications
+  name                = each.key
+  client_id           = each.value.client_id
+  client_secret       = each.value.client_secret
+  authorization_flow  = authentik_flow.provider-authorization-implicit-consent.uuid
+  authentication_flow = authentik_flow.authentication.uuid
+  invalidation_flow   = data.authentik_flow.default-provider-invalidation-flow.id
+  property_mappings = concat(
+    data.authentik_property_mapping_provider_scope.oauth2.ids,
+    [authentik_property_mapping_provider_scope.email_verified.id],
+  )
   access_token_validity = "hours=4"
   signing_key           = data.authentik_certificate_key_pair.generated.id
   allowed_redirect_uris = [
--- terraform/authentik/scopes.tf
+++ terraform/authentik/scopes.tf
@@ -6,3 +6,14 @@ data "authentik_property_mapping_provider_scope" "oauth2" {
     "goauthentik.io/providers/oauth2/scope-profile"
   ]
 }
+
+resource "authentik_property_mapping_provider_scope" "email_verified" {
+  name       = "Email Verified Scope"
+  scope_name = "email"
+  expression = <<-EOT
+    return {
+      "email": user.email,
+      "email_verified": True,
+    }
+  EOT
+}
