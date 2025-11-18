853129b61f149c9ad3dce0431cb97034b20d740e
Jory Irving <jory.irving@stackadapt.com>
2025-11-18 15:03:19 -0700

    fix: cleanup import block

--- terraform/uptimerobot/variables.tf
+++ terraform/uptimerobot/variables.tf
@@ -2,3 +2,8 @@ variable "uptimerobot_api_key" {
   type        = string
   description = "Uptime Robot API Key"
 }
+
+variable "taco_time_discord_webhook_url" {
+  type        = string
+  description = "Taco Time Discord Webhook URL"
+}
