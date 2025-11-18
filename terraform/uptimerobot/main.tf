68ea11f634836d3045b03f9b9e97cdd6beda7686
Jory Irving <jory.irving@stackadapt.com>
2025-11-18 13:34:03 -0700

    feat: add uptimerobot terraform

---
+++ terraform/uptimerobot/main.tf
@@ -0,0 +1,60 @@
+terraform {
+  required_providers {
+    uptimerobot = {
+      source  = "uptimerobot/uptimerobot"
+      version = "1.0.0"
+    }
+  }
+}
+
+provider "uptimerobot" {
+  api_key = var.uptimerobot_api_key
+}
+
+resource "uptimerobot_monitor" "plex" {
+  name     = "Plex"
+  type     = "HTTP"
+  url      = "https://plex.jory.dev"
+  interval = 300
+
+  follow_redirections = true
+
+  timeout = 30
+}
+
+resource "uptimerobot_monitor" "seerr" {
+  name     = "Seerr"
+  type     = "HTTP"
+  url      = "https://requests.jory.dev"
+  interval = 300
+
+  follow_redirections = true
+
+  timeout = 30
+}
+
+resource "uptimerobot_monitor" "gatus" {
+  name     = "Status Page"
+  type     = "HTTP"
+  url      = "https://status.jory.dev"
+  interval = 300
+
+  follow_redirections = true
+
+  timeout = 30
+}
+
+import {
+  to = uptimerobot_monitor.plex
+  id = "795945113"
+}
+
+import {
+  to = uptimerobot_monitor.seerr
+  id = "795945114"
+}
+
+import {
+  to = uptimerobot_monitor.gatus
+  id = "795944990"
+}
---
+++ terraform/uptimerobot/variables.tf
@@ -0,0 +1,4 @@
+variable "uptimerobot_api_key" {
+  type        = string
+  description = "Uptime Robot API Key"
+}
