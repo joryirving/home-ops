bd830567a4b76c36b9b1b823a17a0509f518001d
Miso <miso@jory.dev>
2026-02-24 11:00:08 -0700

    ci: add eslint flat config for eslint v10

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+module.exports = [
+  {
+    ignores: ['node_modules/**', 'coverage/**'],
+  },
+  {
+    files: ['**/*.js'],
+    languageOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'commonjs',
+    },
+    rules: {
+      'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      'no-undef': 'error',
+    },
+  },
+];
